ec7f0f3ff907488a92a727521e3e538eaefa9790
Kairat Batyrbayev <k.batyrbayev>
2021-12-13 14:49:56 +0600

    mat select, mat radio, datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MyDatePickerComponent } from './my-date-picker/my-date-picker.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatRadioModule } from '@angular/material/radio';
-import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -15,8 +20,14 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     FormsModule,
     MatRadioModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my-date-picker/my-date-picker.component.ts
+++ src/app/my-date-picker/my-date-picker.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatRadioChange } from '@angular/material/radio';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-my-date-picker',
@@ -7,16 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyDatePickerComponent implements OnInit {
 
+  radioControl = new FormControl('day');
+  displayTypeControl = new FormControl();
+  dateControl = new FormControl();
+
   group = [
-    { name: 'Сегодня', value: 'day' },
+    { name: 'Сегодня dd.mm.yyyy', value: 'day' },
+    { name: 'Месяц mm.yyyy', value: 'month' },
+    { name: 'Год yyyy', value: 'year' },
+    { name: 'Период dd.mm.yyyy - dd.mm.yyyy', value: 'period' },
+  ];
+  displayTypes = [
+    { name: 'День', value: 'day' },
     { name: 'Месяц', value: 'month' },
     { name: 'Год', value: 'year' },
-    { name: 'Период', value: 'period' },
-  ];
+  ]
+
+  startview: 'month' | 'year' | 'multi-year' = 'multi-year';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRadioChange($event: MatRadioChange) {
+    console.log($event);
+    switch($event.value) {
+      case 'day': this.startview = 'multi-year'; break;
+      case 'month': this.startview = 'year'; break;
+      case 'year': this.startview = 'month'; break;
+      case 'period': this.startview = 'year'; break;
+    }
+  }
+
+  onSelectChange($event: MatSelectChange) {
+    console.log($event);
+  }
+
+  onDateChange($event: any) {
+    console.log($event);
   }
 
 }
